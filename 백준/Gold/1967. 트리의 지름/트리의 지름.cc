4ac93352e701db9c463c166f851e80bf2f25997d
dohoon0122 <57519901+dohoon0122@users.noreply.github.com>
2025-01-22 00:24:48 +0900

    [Gold IV] Title: 트리의 지름, Time: 4 ms, Memory: 2788 KB -BaekjoonHub

---
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.cc
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n;
+    cin>>n;
+    vector<vector<pair<int, int>>> tree(n);
+    for(int i=0; i<n-1; i++){
+        int u, v, w;
+        cin>>u>>v>>w;
+        tree[u-1].push_back({v-1, w});
+        tree[v-1].push_back({u-1, w});
+    }
+    
+    vector<bool> visited(n);
+
+    stack<pair<int, int>> st;
+    visited[0] = true;
+    st.push({0, 0});
+    int maxR = 0;
+    int chNode = 0;
+    
+    while(!st.empty()){
+        auto [curr, R] = st.top();
+        if(R>maxR){
+            maxR = R;
+            chNode = curr;
+        }
+        st.pop();
+        for(auto [next, weight]: tree[curr]){
+            if(!visited[next]){
+                visited[next] = true;
+                st.push({next, weight+R});
+            }
+        }
+    }
+    
+    for(int i=0; i<n; i++){
+        visited[i] = false;
+    }
+    visited[chNode] = true;
+    maxR = 0;
+    st.push({chNode, 0});
+    
+    while(!st.empty()){
+        auto [curr, R] = st.top();
+        if(R>maxR){
+            maxR = R;
+            chNode = curr;
+        }
+        st.pop();
+        for(auto [next, weight]: tree[curr]){
+            if(!visited[next]){
+                visited[next] = true;
+                st.push({next, weight+R});
+            }
+        }
+    }
+    
+    cout<<maxR;
+    
+    return 0;
+}
