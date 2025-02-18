702395b43fd1b66e22b7d156f893bbbb5ff5f2bf
dohoon0122 <57519901+dohoon0122@users.noreply.github.com>
2025-02-19 00:07:03 +0900

    [Bronze IV] Title: 기찍 N, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n;
+    cin>>n;
+    while(n--){
+        cout<<n+1<<"\n";
+    }
+}
